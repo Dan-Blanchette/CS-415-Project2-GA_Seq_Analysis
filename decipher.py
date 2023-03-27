fe23f82645ad07c6ad6303838bb906ec0eae8bda
Taylor <mart8517@vandals.uidaho.edu>
2023-03-27 13:02:59 -0700

    added read_csv

--- decipher.py
+++ decipher.py
@@ -1,12 +1,16 @@
+import pandas as pd
+
 def load_data():
     """Load the data from the file into a list of strings."""
-    
+    df = pd.read_csv('datafile.csv')
+    print(df)
 
-def decipher(data):
+def decipher(df):
     """Decipher the data."""
 
 def main():
     """Main function for deciphering a sequence of DNA."""
+    load_data()
 
 if __name__ == 'main':
     main()
