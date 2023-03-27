5988c00a7fe87d2c97a6b690903963c34d93ecf9
Taylor <mart8517@vandals.uidaho.edu>
2023-03-27 12:58:21 -0700

    created decipher.py; added boilerplate code

---
+++ decipher.py
@@ -0,0 +1,12 @@
+def load_data():
+    """Load the data from the file into a list of strings."""
+    
+
+def decipher(data):
+    """Decipher the data."""
+
+def main():
+    """Main function for deciphering a sequence of DNA."""
+
+if __name__ == 'main':
+    main()
