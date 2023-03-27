9025266d1e96dab818461d691e5930c6a92cc662
Jordan Reed <reed5204@vandals.uidaho.edu>
2023-03-27 12:25:20 -0700

    Added code to global alignment and added blosum matrix

--- glob_align.py
+++ glob_align.py
@@ -1,35 +1,48 @@
+# --------
+# @file     glob_align.py
+# @author   Jordan Reed, Dan Blanchette, Taylor Martin
+# @date     March 2023
+# @class    CS 415 Computational Biology
+#
+# @brief    This file is for the gloabl alignment algorithm.
+# --------------------
+
+import blosum
+
 def printmatrix(m,pad = 4): 
     for r in m:
         for d in r:
-            print(f"{str(d):>{pad}}", end = " ")
+            print(f"{str(d):>{pad}}", end = "")
         print()
 
 seq1 = "TTAGCCAGT"
 seq2 = "TAAGACATTTTAC"
-gappenalty = -8
-scoringmatrix = [[00 for i in range(0,len(seq1)+1)] for j in range(0,len(seq2)+1)]
-directionmatrix = [["." for i in range(0,len(seq1)+1)] for j in range(0,len(seq2)+1)]
-print(scoringmatrix)
-scoringmatrix[0][1] = 77
-print(scoringmatrix)
-printmatrix(scoringmatrix)
 
+gappenalty = -8
+
+scoringmatrix = [[0 for i in range(0,len(seq1)+1)] for j in range(0,len(seq2)+1)]
+directionmatrix = [["." for i in range(0,len(seq1)+1)] for j in range(0,len(seq2)+1)]
+
+
+# fill first row with gap penalty
 for i in range(0,len(scoringmatrix[0])):
     scoringmatrix[0][i] =  i*gappenalty
-    directionmatrix[0][i] = "h "
+    directionmatrix[0][i] = "\u2190" # horizontal
+
+# firll first column
 for i in range(0,len(scoringmatrix)):
     scoringmatrix[i][0] = i*gappenalty
-    directionmatrix[i][0]  = "v "
+    directionmatrix[i][0]  = "\u2191" # vertical
 
-for r in range(1,len(scoringmatrix)):
-    for c in range(1,len(scoringmatrix[r])):
+# fill in rest of table
+for r in range(1, len(scoringmatrix)):
+    for c in range(1, len(scoringmatrix[0])):
         vert = scoringmatrix[r-1][c] + gappenalty
         horz = scoringmatrix[r][c-1] + gappenalty
         diag = scoringmatrix[r-1][c-1]
-        if(seq1[c-1] == seq2[r-1]):
-            diag += 10
-        else:
-            diag -= 10
+
+        diag += blosum.blosum50[blosum.aminoDictionary[seq2[r-1]]][blosum.aminoDictionary[seq1[c-1]]]
+        
         scoringmatrix[r][c] = max(vert,horz,diag)
         #directionmatrix[r][c] = "v or h or d"
         if diag >= horz and diag >= vert:
@@ -39,8 +52,40 @@ for r in range(1,len(scoringmatrix)):
         if vert > diag and vert > horz:
             directionmatrix[r][c] = "\u2191"
 
-printmatrix(scoringmatrix,4)
+printmatrix(scoringmatrix)
 print(seq1)
 print(seq2)
 
-printmatrix(directionmatrix,0)
+printmatrix(directionmatrix)
+
+# aligning sequences
+
+aligned1 = ""
+aligned2 = ""
+pos1 = len(seq1)
+pos2 = len(seq2)
+
+while pos1 > 0 or pos2 > 0:
+    if directionmatrix[pos2][pos1] == '\u2196': # diagonal
+        aligned1 += seq1[pos1-1]
+        aligned2 += seq2[pos2-1]
+        pos1 -= 1
+        pos2 -=1
+    elif directionmatrix[pos2][pos1] == '\u2190': # horizontal
+        aligned1 += seq1[pos1-1]
+        aligned2 += "-"
+        pos1 -= 1
+    elif directionmatrix[pos2][pos1] == '\u2191': # vertical
+        aligned1 += "-"
+        aligned2 += seq2[pos2-1]
+        # pos1 -= 1
+        pos2 -=1
+    else:
+        print("something is wrong")
+
+
+aligned1 = aligned1[::-1]
+aligned2 = aligned2[::-1]
+
+print(aligned1)
+print(aligned2)
