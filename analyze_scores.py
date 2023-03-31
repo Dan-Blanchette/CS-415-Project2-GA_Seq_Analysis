a423d7560cada0f08fa36b5c2fa6d2bef13750c6
Taylor <tmartin6894@gmail.com>
2023-03-31 12:34:43 -0700

    seperated code into modular functions

--- analyze_scores.py
+++ analyze_scores.py
@@ -41,63 +41,74 @@ def calculate_accuracy(group1, group2, group3):
     print(f'right {right} wrong {wrong} total: {right+wrong}')
     print(f'accuracy: {right/(right+wrong)}')
 
-f = pd.read_csv("datafile.csv",usecols=[1])
+
+def create_table():
+    f = pd.read_csv("datafile.csv",usecols=[1])
 
 
-table = [[0 for i in range(0,len(f))] for j in range(0,len(f))]
+    table = [[0 for i in range(0,len(f))] for j in range(0,len(f))]
 
 
-for i in range(0, len(f['Genome_Sequence'])):
-    for j in range(0, len(f['Genome_Sequence'])):
-        # if(i != j):
-            val = ga.align_seq(f['Genome_Sequence'][i], f['Genome_Sequence'][j])
-            table[i][j] = val
+    for i in range(0, len(f['Genome_Sequence'])):
+        for j in range(0, len(f['Genome_Sequence'])):
+            # if(i != j):
+                val = ga.align_seq(f['Genome_Sequence'][i], f['Genome_Sequence'][j])
+                table[i][j] = val
+    return table
 
-# make_graph(table)
+def analyze_scores(table):
+
+    # make_graph(table)
+
+    # following code works with 76% accuracy ie identifies low mutation rate really well
+
+    group1 = []
+    group2 = []
+    group3 = []
+
+    # percentage = .82
+    # top 35%
+    for i in range(0,50): # row
+        for j in range(0,50): # column
+            if i != j and table[i][j] != 0:
+                if (table[i][j]) >= (table[i][i]*.6) :
+                    if i not in group1:
+                        group1.append(i)
+                    if j not in group1:
+                        group1.append(j)
+
+    for i in range(0,50):
+        if i in group1:
+            for j in range(0,50):
+                table[i][j] = 0
+    group1.sort()
+    print(group1, len(group1))
+    for i in range(0,50): # row
+        if i not in group1:
+            val = np.mean(table[i])
+            if val < table[i][i]*.35 and i not in group2:
+                group2.append(i)
+
+    for i in range(0,50):
+        if i in group2:
+            for j in range(0,50):
+                table[i][j] = 0
+    group2.sort()
+    print(group2, len(group2))
+
+    for i in range(0, 50):
+        if table[i][0] != 0 and table[i][1] != 0:
+            group3.append(i)
+
+    print(group3, len(group3))
+
+    return group1, group2, group3
 
 
-# following code works with 76% accuracy ie identifies low mutation rate really well
-import numpy as np
+def main():
+    table = create_table()
+    group1, group2, group3 = analyze_scores(table)
+    calculate_accuracy(group1, group2, group3)
 
-
-group1 = []
-group2 = []
-group3 = []
-
-# percentage = .82
-# top 35%
-for i in range(0,50): # row
-    for j in range(0,50): # column
-        if i != j and table[i][j] != 0:
-            if (table[i][j]) >= (table[i][i]*.6) :
-                if i not in group1:
-                    group1.append(i)
-                if j not in group1:
-                    group1.append(j)
-
-for i in range(0,50):
-    if i in group1:
-        for j in range(0,50):
-            table[i][j] = 0
-group1.sort()
-print(group1, len(group1))
-for i in range(0,50): # row
-    if i not in group1:
-        val = np.mean(table[i])
-        if val < table[i][i]*.35 and i not in group2:
-            group2.append(i)
-
-for i in range(0,50):
-    if i in group2:
-        for j in range(0,50):
-            table[i][j] = 0
-group2.sort()
-print(group2, len(group2))
-
-for i in range(0, 50):
-    if table[i][0] != 0 and table[i][1] != 0:
-        group3.append(i)
-
-print(group3, len(group3))
-
-calculate_accuracy(group1, group2, group3)
+if __name__ == "__main__":
+    main()
